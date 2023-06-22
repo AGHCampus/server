11b34650b817cd25425a9d870d3328a516c77515
maati01 <mateuszpow7@gmail.com>
2023-06-23 01:54:46 +0200

    added information schema and entity

---
+++ src/main/java/pl/edu/agh/server/model/Information.java
@@ -0,0 +1,20 @@
+package pl.edu.agh.server.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "information")
+public class Information {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @CreationTimestamp
+    private Date createdDate;
+
+    private String content;
+    private String author;
+}
