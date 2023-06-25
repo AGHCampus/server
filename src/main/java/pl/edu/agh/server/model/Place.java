80f282c3db8b40bd597cbb7b3db7af3317fa15b8
maati01 <mateuszpow7@gmail.com>
2023-06-26 01:18:30 +0200

    fix

--- src/main/java/pl/edu/agh/server/model/Place.java
+++ src/main/java/pl/edu/agh/server/model/Place.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class Place {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    public  Long id;
+    private  Long id;
 
     @OneToOne(cascade = CascadeType.ALL)
     @PrimaryKeyJoinColumn
