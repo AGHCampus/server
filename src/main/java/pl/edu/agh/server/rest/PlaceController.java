dcc505b9ecce659351cae2a9a37e0c24086e3fac
maati01 <mateuszpow7@gmail.com>
2023-10-21 23:21:54 +0200

    added all data endpoint for place

--- src/main/java/pl/edu/agh/server/rest/PlaceController.java
+++ src/main/java/pl/edu/agh/server/rest/PlaceController.java
@@ -5,13 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.edu.agh.server.common.LocationContent;
+import pl.edu.agh.server.model.Event;
+import pl.edu.agh.server.model.Offer;
 import pl.edu.agh.server.model.Place;
 import pl.edu.agh.server.model.PlaceDetails;
-import pl.edu.agh.server.model.PlaceEvent;
-import pl.edu.agh.server.model.PlaceOffer;
 import pl.edu.agh.server.repostiory.PlaceDetailsRepository;
-import pl.edu.agh.server.repostiory.PlaceEventRepository;
-import pl.edu.agh.server.repostiory.PlaceOfferRepository;
 import pl.edu.agh.server.repostiory.PlaceRepository;
 
 import java.util.List;
@@ -22,8 +21,9 @@ import java.util.List;
 public class PlaceController {
     private final PlaceRepository placeRepository;
     private final PlaceDetailsRepository placeDetailsRepository;
-    private final PlaceOfferRepository placeOfferRepository;
-    private final PlaceEventRepository placeEventRepository;
+    private final EventController eventController;
+    private final OfferController offerController;
+
 
     @GetMapping(value = "/", produces = "application/json")
     public List<Place> getPlacesList() {
@@ -35,13 +35,12 @@ public class PlaceController {
         return placeDetailsRepository.findById(id).orElse(null);
     }
 
-    @GetMapping(value = "/{id}/offers", produces = "application/json")
-    public List<PlaceOffer> getOffersList(@PathVariable long id) {
-        return placeOfferRepository.findByPlaceId(id);
-    }
+    @GetMapping(value = "/{id}/info", produces = "application/json")
+    public LocationContent getAllData(@PathVariable long id) {
+        List<Offer> offers = offerController.getPlaceOffers(id);
+        List<Event> events = eventController.getPlaceEvents(id);
+        PlaceDetails placeDetails = getPlaceDetails(id);
 
-    @GetMapping(value = "/{id}/events", produces = "application/json")
-    public List<PlaceEvent> getEventsList(@PathVariable long id) {
-        return placeEventRepository.findByPlaceId(id);
+        return new LocationContent(offers, events, placeDetails);
     }
 }
